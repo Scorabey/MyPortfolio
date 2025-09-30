e96f2c0581520637fd63cd6f898fd153d55a8eca
Scorabeyko <satlovdanijl@gmail.com>
2025-09-30 21:05:56 +0300

    Fix preview htmls

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	// base: "",
-	build: {
-		rollupOptions: {
-			input: {
-				main: "index.html",
-			},
-		},
-	},
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './Index.html'),
+        about: resolve(__dirname, './About.html'),
+        contacts: resolve(__dirname, './Connect.html'),
+        portfolio: resolve(__dirname, './Projects.html'),
+      }
+    }
+  }
+})
