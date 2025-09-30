0056c9e1ce678a65e198129d8483a0d276c2d5ef
Scorabeyko <satlovdanijl@gmail.com>
2025-09-30 20:20:07 +0300

    first commit

---
+++ src/Scripts/Connect.js
@@ -0,0 +1,34 @@
+import "../Style/Connect/Connect-main.scss"
+
+const header = document.getElementById('header');;
+
+window.addEventListener("scroll", () => {
+    const scrollPos = window.scrollY;
+
+    if(scrollPos > 100) 
+        {
+            header.classList.add('header-script');
+        }else 
+        {
+            header.classList.remove('header-script');
+        }
+})
+
+document.querySelectorAll('li').forEach(
+    li => {
+        li.addEventListener('click', () => {
+            const link = li.querySelector("a");
+            if(link) {
+                window.location = link.href;
+            }
+        })
+    }
+)
+
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    if(preloader) {
+        preloader.classList.add('done')
+        setTimeout(() => preloader.parentElement.removeChild(preloader), 1000)
+    }
+})
